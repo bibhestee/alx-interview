aafd50a31e598ced8a22ec58b2319ec4be692a33
Bibest <bibhestee@gmail.com>
2023-07-01 16:44:26 +0100

    returns pascal triangle coefficients as a list
    each column contains a the coeffs in a row

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    """
+        pascal_triangle : function that returns a pascal triangle
+
+        Args:
+            n (int) : number of rows
+        Return:
+            list of pascal coefficients by row
+    """
+    pascal_list = []
+    if n <= 0:
+        return pascal_list
+    for i in range(1, n + 1):
+        coeff = 1
+        row = []
+        for j in range(1, i + 1):
+            row.append(str(coeff))
+            coeff = coeff * (i - j) // j
+        pascal_list.append(row)
+    return pascal_list
