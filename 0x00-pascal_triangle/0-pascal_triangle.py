1e2e29249b5fef0e6b23be664995f5efdba2ecf9
Bibest <bibhestee@gmail.com>
2023-07-01 22:35:17 +0100

    modify the return statement

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
    """
     pascal_list = []
     if n <= 0:
-        return pascal_list
+        return (pascal_list)
     for i in range(1, n + 1):
         coeff = 1
         row = []
@@ -14,4 +14,4 @@ def pascal_triangle(n):
             row.append(coeff)
             coeff = coeff * (i - j) // j
         pascal_list.append(row)
-    return pascal_list
+    return (pascal_list)
