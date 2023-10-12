6dda9574130efb9531fa0faf9470af568309dd59
Bibest <bibhestee@gmail.com>
2023-10-12 16:19:32 +0000

    Method implemented for minimum number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+This file contains a script to calculate the minimum or
+fewest number of operations neded to result in exactly n 'H'
+characters in the file
+"""
+
+
+def minOperations(n):
+    """
+        Function_Name - minOperations
+        Arguments:
+            n ( Integer )
+        Returns:
+            Integers
+    """
+    num_of_operations = 0
+    clipboard = 0
+    while(n > 1):
+        if (clipboard > 0 and n/clipboard):
+            n = n//clipboard
+            num_of_operations += clipboard
+        else:
+            f = largestFactor(n)
+            n = n//f
+            num_of_operations += f
+            clipboard = f
+    return num_of_operations
+
+
+def largestFactor(n):
+    factor = 1
+    for i in range(1, n):
+        if (n % i == 0):
+            factor = i
+    return factor
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,9 @@
+# 0X02. Minimum Operations
+
+
+## Concepts
+- Python
+- Algorithm
+
+## Task
+1 - Minimum operation in a text file
